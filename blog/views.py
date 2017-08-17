5caab91781e7dec56081e68096cc6065fb56a0d2
kyriale <kyriale3661@gmail.com>
2017-08-17 17:50:42 +0900

    finish 2

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,6 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
